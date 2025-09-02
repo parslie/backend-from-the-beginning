2edef9358902acc5ef98d78ecd99d9204133d4d6
Viktor Holta <viktor.holta@outlook.com>
2025-09-02 19:40:28 +0200

    Implement server specified in "5.2. server"

--- server/main.go
+++ server/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
+func echoUpper(w io.Writer, r io.Reader) {
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Fprintf(w, "%s\n", strings.ToUpper(line))
+	}
+	if err := scanner.Err(); err != nil {
+		log.Printf("error: %s", err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	const name = "bftb-tcpupperecho"
+	log.SetPrefix(name + "\t")
+
+	port := flag.Int("p", 8080, "port to listen on")
+	flag.Parse()
+
+	listener, err := net.ListenTCP("tcp", &net.TCPAddr{Port: *port})
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	log.Printf("listening at localhost: %s", listener.Addr())
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go echoUpper(conn, conn)
+	}
 }
