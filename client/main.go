6ceaeb4356fb2676004477b35d278a26f55a5a7b
Viktor Holta <viktor.holta@outlook.com>
2025-09-02 19:18:16 +0200

    Implement client specified in "5.1. client"

--- client/main.go
+++ client/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	const name = "bftb-writetcp"
+	log.SetPrefix(name + "\t")
+
+	port := flag.Int("p", 8080, "port to connect to")
+	flag.Parse()
+
+	conn, err := net.DialTCP("tcp", nil, &net.TCPAddr{Port: *port})
+	if err != nil {
+		log.Fatalf("error connecting to localhost:%d: %v", *port, err)
+	}
+	log.Printf("connected to %s: will forward stdin", conn.RemoteAddr())
+	defer conn.Close()
+
+	go func() {
+		for connScanner := bufio.NewScanner(conn); connScanner.Scan(); {
+			fmt.Printf("%s\n", connScanner.Text())
+
+			if err := connScanner.Err(); err != nil {
+				log.Fatalf("error reading from %s: %v", conn.RemoteAddr(), err)
+			}
+		}
+	}()
+
+	for stdinScanner := bufio.NewScanner(os.Stdin); stdinScanner.Scan(); {
+		log.Printf("sent: %s\n", stdinScanner.Text())
+		if _, err := conn.Write(stdinScanner.Bytes()); err != nil {
+			log.Fatalf("error writing to %s: %v", conn.RemoteAddr(), err)
+		}
+		if _, err := conn.Write([]byte("\n")); err != nil {
+			log.Fatalf("error writing to %s: %v", conn.RemoteAddr(), err)
+		}
+		if stdinScanner.Err() != nil {
+			// NOTE: It's not the remote address we're reading from???
+			log.Fatalf("error reading from %s: %v", conn.RemoteAddr(), err)
+		}
+	}
 }
